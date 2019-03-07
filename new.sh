08c68a6ba59926f2637aa54b8bff57cf369ad180
gy741 <gy741.kim@gmail.com>
2019-03-07 23:20:47 +0900

    Update new.sh

--- new.sh
+++ new.sh
@@ -22,7 +22,7 @@ go get github.com/haccer/subjack;
 go get github.com/ffuf/ffuf;
 mkdir -pv ~/bounty/tools/aquatone;
 wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip -O ~/bounty/tools/aquatone/aquatone.zip;
-uzip ~/bounty/tools/aquatone/aquatone.zip -d ~/bounty/tools/aquatone; # Unzip aquatone
+unzip ~/bounty/tools/aquatone/aquatone.zip -d ~/bounty/tools/aquatone; # Unzip aquatone
 git clone https://github.com/rbsec/dnscan.git ~/bounty/tools/dnscan;
 git clone https://github.com/infosec-au/altdns.git ~/bounty/tools/altdns; 
 git clone https://github.com/blechschmidt/massdns.git ~/bounty/tools/massdns; 
