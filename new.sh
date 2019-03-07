c7dd3ec53256b3cb5c6b240bf15024aa4e5e94eb
gy741 <gy741.kim@gmail.com>
2019-03-07 08:39:04 -0500

    Add files via upload

--- new.sh
+++ new.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# Colors
+NC='\033[0m';
+RED='\033[0;31m';
+GREEN='\033[0;32m';
+BLUE='\033[0;34m';
+ORANGE='\033[0;33m';
+
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6
 echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
 sudo apt-get update -m
-sudo apt install unzip curl sublist3r masscan nmap nikto gobuster chromium-browser python-pip python3-pip p7zip-full  whatweb wafw00f -y;
+
+sudo apt-get install unzip git gcc g++ wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full -y;
 echo -e "$GREEN""Installing Go from golang.org.""$NC";
 wget https://dl.google.com/go/go1.12.linux-amd64.tar.gz;
 sudo tar -C /usr/local -xzf go1.12.linux-amd64.tar.gz;
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:" >> "$HOME"/.profile;
 source "$HOME"/.profile;
 rm -rf go1.12.linux-amd64.tar.gz;
-go get github.com/subfinder/subfinder;
-go get github.com/haccer/subjack;
-go get github.com/ffuf/ffuf;
-mkdir -pv ~/bounty/tools/aquatone;
-wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip -O ~/bounty/tools/aquatone/aquatone.zip;
-uzip ~/bounty/tools/aquatone/aquatone.zip -d ~/bounty/tools/aquatone; # Unzip aquatone
-git clone https://github.com/rbsec/dnscan.git ~/bounty/tools/dnscan;
-git clone https://github.com/infosec-au/altdns.git ~/bounty/tools/altdns; 
-git clone https://github.com/blechschmidt/massdns.git ~/bounty/tools/massdns; 
-cd ~/bounty/tools/massdns; make; # Compiling massdns, see repo for details
-git clone https://github.com/mazen160/bfac.git ~/bounty/tools/bfac;
+
+
+
+echo -e "$GREEN""Please run 'source ~/.profile' to add the Go binary path to your \$PATH variable, then run Chomp Scan.""$NC";
+echo -e "$ORANGE""Note: In order to use S3Scanner, you must configure your personal AWS credentials in the aws CLI tool.""$NC";
+echo -e "$ORANGE""See https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html for details.""$NC";
