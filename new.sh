0f895ee46cfbabd36b43ed05b9c1ed7a24786498
gy741 <gy741.kim@gmail.com>
2019-03-09 14:45:53 +0900

    Update new.sh

--- new.sh
+++ new.sh
@@ -10,7 +10,7 @@ ORANGE='\033[0;33m';
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
 echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
 apt-get update -m
-apt-get install sublist3r gobuster unzip make gcc g++ wget git curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full -y;
+apt-get install sublist3r unzip make gcc g++ wget git curl chromium-browser python-pip python3-pip p7zip-full -y;
 echo -e "$GREEN""Installing Go from golang.org.""$NC";
 wget https://dl.google.com/go/go1.12.linux-amd64.tar.gz;
 sudo tar -C /usr/local -xzf go1.12.linux-amd64.tar.gz;
@@ -18,8 +18,7 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:" >> "$HOME"/.profile;
 source "$HOME"/.profile;
 rm -rf go1.12.linux-amd64.tar.gz;
 go get github.com/subfinder/subfinder;
-go get github.com/haccer/subjack;
-go get github.com/ffuf/ffuf;
+go get github.com/gy741/subjack;
 mkdir -pv ~/bounty/tools/aquatone;
 wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip -O ~/bounty/tools/aquatone/aquatone.zip;
 unzip ~/bounty/tools/aquatone/aquatone.zip -d ~/bounty/tools/aquatone; # Unzip aquatone
@@ -27,9 +26,8 @@ git clone https://github.com/rbsec/dnscan.git ~/bounty/tools/dnscan;
 git clone https://github.com/infosec-au/altdns.git ~/bounty/tools/altdns; 
 git clone https://github.com/blechschmidt/massdns.git ~/bounty/tools/massdns; 
 cd ~/bounty/tools/massdns; make; # Compiling massdns, see repo for details
-git clone https://github.com/mazen160/bfac.git ~/bounty/tools/bfac;
-pip2 install -r requirements2.txt;
-pip3 install -r requirements3.txt
+pip2 install -r ./requirements2.txt;
+pip3 install -r ./requirements3.txt
 
 
 
