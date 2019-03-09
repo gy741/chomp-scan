e1cf94e29602815681125839548d4d9890041fe4
gy741 <gy741.kim@gmail.com>
2019-03-09 15:21:44 +0900

    Update new.sh

--- new.sh
+++ new.sh
@@ -22,6 +22,9 @@ go get github.com/gy741/subjack;
 mkdir -pv ~/bounty/tools/aquatone;
 wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip -O ~/bounty/tools/aquatone/aquatone.zip;
 unzip ~/bounty/tools/aquatone/aquatone.zip -d ~/bounty/tools/aquatone; # Unzip aquatone
+wget https://downloads.rclone.org/v1.46/rclone-v1.46-linux-amd64.zip -O ~/rclone;
+unzip ~/rclone/rclone.zip -d ~/rclone/; 
+
 git clone https://github.com/rbsec/dnscan.git ~/bounty/tools/dnscan;
 git clone https://github.com/infosec-au/altdns.git ~/bounty/tools/altdns; 
 git clone https://github.com/blechschmidt/massdns.git ~/bounty/tools/massdns; 
