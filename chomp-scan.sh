3dc0c2bf7dcbed990ccd3b803cf3da0187e80fc8
gy741 <gy741.kim@gmail.com>
2019-05-18 22:15:15 +0900

    Update chomp-scan.sh

--- chomp-scan.sh
+++ chomp-scan.sh
@@ -984,7 +984,8 @@ function run_massdns() {
 				echo -e "$GREEN""[i]$ORANGE Command: cat (all found domains and IPs) | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -q -t A -o S -w $WORKING_DIR/massdns-result.txt.""$NC";
 				START=$(date +%s);
 				cat "$WORKING_DIR"/$ALL_DOMAIN "$WORKING_DIR"/$ALL_IP "$WORKING_DIR"/massdns-appended.txt | sort | uniq | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -q -t A -o S -w "$WORKING_DIR"/massdns-result.txt;
-				$MASSDNS_BRUTE $3 $1 | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -t A -o S -w massdns_brute_output.txt 
+				$MASSDNS_BRUTE $3 $1 | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -t A -o S -w "$WORKING_DIR"/massdns_brute_output.txt; 
+				cat "$WORKING_DIR"/massdns_brute_output.txt >> "$WORKING_DIR"/massdns-result.txt;
 				END=$(date +%s);
 				DIFF=$(( END - START ));
 		else
@@ -998,7 +999,8 @@ function run_massdns() {
 				echo -e "$GREEN""[i]$ORANGE Command: cat (all found domains and IPs) | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -q -t A -o S -w $WORKING_DIR/massdns-result.txt.""$NC";
 				START=$(date +%s);
 				cat "$WORKING_DIR"/$ALL_DOMAIN "$WORKING_DIR"/$ALL_IP "$WORKING_DIR"/goaltdns-output.txt "$WORKING_DIR"/massdns-appended.txt | sort | uniq | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -q -t A -o S -w "$WORKING_DIR"/massdns-result.txt;
-				$MASSDNS_BRUTE $3 $1 | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -t A -o S -w massdns_brute_output.txt 
+				$MASSDNS_BRUTE $3 $1 | $MASSDNS_BIN -r $MASSDNS_RESOLVERS -t A -o S -w "$WORKING_DIR"/massdns_brute_output.txt; 
+				cat "$WORKING_DIR"/massdns_brute_output.txt >> "$WORKING_DIR"/massdns-result.txt;
 				END=$(date +%s);
 				DIFF=$(( END - START ));
 		fi
