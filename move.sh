18f465ea2d7ffaec6b92c9e047be9d34e1003b61
gy741 <gy741.kim@gmail.com>
2019-08-25 00:22:05 +0900

    Add files via upload

---
+++ move.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+mv chomp-scan/output/awscheck_* ~/backup_ns/
+mv chomp-scan/output/yellow_* ~/backup_ns/
+mv chomp-scan/output/red_* ~/backup_ns/
+mv chomp-scan/output/orange_* ~/backup_ns/
+mv chomp-scan/output/FATAL_ERROR*  ~/backup_ns/
+
